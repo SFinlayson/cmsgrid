da92ed235ae68dd2793cf992c5e3709470cd26b6
Stephen Finlayson <finlaysonsteve@gmail.com>
2022-09-05 09:54:51 +0100

    Automatic commit from Steve Finlayson
    Signed-off-by: Stephen Finlayson <finlaysonsteve@gmail.com>

--- cmsgrid/forms.py
+++ cmsgrid/forms.py
@@ -1,63 +1,24 @@
+from django import forms
 from .models import GridCell
 
 
-class Grid(CMSPlugin):
-    """
-    A plugin that has sub Column classes
-    """
+# ############################################################################
+# HTML Grid
 
-    label = models.CharField(max_length=80, blank=True, null=True)
-    class_name = models.CharField(max_length=80)
-    template_areas = models.TextField(
-        blank=True, null=True,
-        help_text="Encapsulate rows with quotes")
-    grid_template_columns = models.CharField(
-        max_length=255, blank=True, null=True,
-        help_text="1fr 10px 1fr")
-    grid_template_rows = models.CharField(
-        max_length=255, blank=True, null=True)
+class GridForm(forms.ModelForm):
 
-    attributes = AttributesField(
-        verbose_name='Attributes',
-        blank=True,
-        excluded_keys=['href', 'target'],
-    )
-    cmsplugin_ptr = models.OneToOneField(
-        CMSPlugin,
-        related_name='%(app_label)s_%(class)s',
-        parent_link=True,
-        on_delete=models.CASCADE,
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        if self.instance.pk is not None:
+            self.fields['create'].initial = 0
+
+    create = forms.IntegerField(
+        label="Create # Grid items",
+        help_text="Create this number of grid cells",
     )
 
-    def __str__(self):
-        if self.label:
-            return self.label
-        else:
-            return "Grid"
-
-
-class GridCell(CMSPlugin):
-    """
-    A cell for the Grids Plugin
-    """
-    label = models.CharField(max_length=80, blank=True, null=True)
-    gridarea = models.CharField(max_length=80, blank=True, null=True)
-
-    attributes = AttributesField(
-        verbose_name='Attributes',
-        blank=True,
-        excluded_keys=['href', 'target']
-    )
-    cmsplugin_ptr = models.OneToOneField(
-        CMSPlugin,
-        related_name='%(app_label)s_%(class)s',
-        parent_link=True,
-        on_delete=models.CASCADE,
-    )
-
-    def __str__(self):
-        if self.label:
-            return self.label
-        else:
-            return "GridItem"
-
+    class Meta:
+        model = GridCell
+        exclude = ('page', 'position', 'placeholder',
+                   'language', 'plugin_type')
